11b08d9595fb0037c58976280939b77cf18d6298
Bharatdwaj3 <badrikedar.jaydutt.456@gmail.com>
2026-01-01 19:08:13 +0530

    added types to user_Controller

--- backend/controller/user.controller.ts
+++ backend/controller/user.controller.ts
@@ -4,41 +4,14 @@ import User  from "../models/user.model.js";
 import { setAccessToken, setRefreshToken, clearAuthCookies, revokeRefreshToken, refreshTokenHandler, destroySession } from '../middleware/token.middleware.js';
 import { sendVerificationEmail } from '../middleware/email.middleware.js';
 import type { RequestHandler } from "express";
+import {
+  req_Registeration,
+  req_Login,
+  res_Registration,
+  res_Login
+} from '../types.js';
 
-
-interface res_Registration{
-  success: boolean,
-  message: String,
-  code: String
-};
-
-interface res_Profile{
-  success: boolean,
-  message: String,
-  code: String
-};
-
-interface res_OAuth_Discord{
-  success: boolean,
-  message: String,
-  code: String
-};
-
-interface res_OAuth_Google{
-  success: boolean,
-  message: String,
-  code: String
-};
-
-interface res_Login{
-  success: boolean,
-  message: String,
-  code: String
-};
-
-
-
-const registerUser:RequestHandler=async(req, res:res_Registration)=>{
+const registerUser:RequestHandler=async(req:req_Registeration, res:res_Registration)=>{
   try{
       const {userName, fullName, email, accountType, password}= req.body;
 
@@ -91,7 +64,7 @@ const registerUser:RequestHandler=async(req, res:res_Registration)=>{
   }
 };
 
-const loginUser:RequestHandler=async(req, res)=>{
+const loginUser:RequestHandler=async(req: req_Registeration, res:res_Login)=>{
   try{
     const {email, password}=req.body;
     if(!email || !password){
--- backend/types.ts
+++ backend/types.ts
@@ -1,45 +1,55 @@
-//Request Type
+import { Request, Response } from 'express';
 
-export interface req_Registeration {
-    userName:string;
-    fullName:string;
-    email:string;
-    password:string;
-    googleId:null|string;
-    discordId:null|string;
-    avatar:string;
-    accountType:string;
-    isEmailVerified:boolean;
-    isActive:boolean;
+//Request Types
+
+export interface req_Registeration extends Request {
+    body:{
+        userName:string;
+        fullName:string;
+        email:string;
+        password:string;
+        googleId?:null|string;
+        discordId?:null|string;
+        avatar?:string;
+        accountType?:string;
+        isEmailVerified?:boolean;
+        isActive?:boolean;
+    }
 }
 
-export interface req_Login {
-    email:string;
-    password:string;
+export interface req_Login extends Request {
+    body:{
+        email:string;
+        password:string;
+    }
 }
 
 //Response Types
 
-export interface res_Registration {
-    success:boolean;
-    message:string;
-    requiresVerfication: boolean;
-    user:{
-        id:string;
-        email: string;
-    };
+export interface res_Registration extends Response{
+    json(body:{
+        success:boolean;
+        message:string;
+        requiresVerfication: boolean;
+        user:{
+            id:string;
+            email: string;
+        };
+    }):this;
 }
 
-export interface res_Login {
-    success: boolean;
-    message: string;
-    user:    {
-        id:string;
-        userName:string;
-        fullName:string;
-        email:string;
-        accountType:string;
-        lastLogin:Date;
-    };
+export interface res_Login extends Response {
+    json(body:{
+        success: boolean;
+        message: string;
+        user:    {
+            id:string;
+            userName:string;
+            fullName:string;
+            email:string;
+            accountType:string;
+            lastLogin:Date;
+        };
+    }):this;
 }
 
